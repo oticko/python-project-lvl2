2fdb4a2e7072d23dac95cde022241a8b7abcb0b8
Otari Maglaperidze <oticko@yandex.ru>
2021-06-19 00:07:37 +0300

    add gendiff

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
