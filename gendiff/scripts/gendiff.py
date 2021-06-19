2f4c339c324912394cb76344500687201a0384c8
Otari Maglaperidze <oticko@yandex.ru>
2021-06-19 14:42:28 +0300

    update project

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
 import argparse
 
 
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f FORMAT', action='help',
+                    help='--format FORMAT set format of output')
 
-args = parser.parse_args()
+
+def main():
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
